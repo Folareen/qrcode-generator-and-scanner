8859d1696334121eb335a1bca49fb74e8bb38dd4
Folareen <sakawahab03@gmail.com>
2022-09-02 03:37:51 +0100

    routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import GenerateCode from './pages/GenerateCode'
+import Homepage from './pages/Homepage'
+import ScanCode from './pages/ScanCode'
 
-function App() {
-  return (
-    <Button colorScheme='blue'>Button</Button>
-  )
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path='/' element={<Homepage/>} />
+          <Route path='/generate-code' element={<GenerateCode/>} />
+          <Route path='/scan-code' element={<ScanCode/>} />
+        </Routes>
+      </Router>
+    )
+  }
 }
-
-export default App
